3d8edb88cd84275b1172eac2dca29fc4bfba5baf
Kris Wong <krischwong@gmail.com>
2023-11-16 00:53:27 -0500

    Added ship locations, missed shots, and ship objects to the gameboard. Added checking to see if the ships are on or off the board and if ships overlap. Added addShips to ensure the ships are valid and to push the current location to the gameboard object and to the ship itself. Added receiveAttack Added allSunk

--- src/Gameboard/Gameboard.js
+++ src/Gameboard/Gameboard.js
@@ -1,6 +1,9 @@
 class Gameboard {
   constructor() {
     this.board = this.createBoard();
+    this.shipLocations = [];
+    this.missed = [];
+    this.allShips = [];
   }
 
   createBoard() {
@@ -11,7 +14,7 @@ class Gameboard {
     return newBoard;
   }
 
-  isValid(initialLocation, direction, ship) {
+  isValidShip(initialLocation, direction, ship) {
     let first = initialLocation;
     let last;
     if (direction == "n") last = first - 10 * (ship.length - 1);
@@ -19,43 +22,101 @@ class Gameboard {
     if (direction == "s") last = first + 10 * (ship.length - 1);
     if (direction == "w") last = first - (ship.length - 1);
 
-    let valid;
+    //Makes sure the ships are placed on the board and don't go off it
+    let validLocation;
     //prettier-ignore
-    if (direction == "n" || direction == "s") valid = ((first >= 0 && first <= 99) && (last>= 0 && last <=100)) ? true: false
+    if (direction == "n" || direction == "s") validLocation = ((first >= 0 && first <= 99) && (last>= 0 && last <=100)) ? true: false
     if (direction == "e" || direction == "w")
-      valid = Math.floor(first / 10) == Math.floor(last / 10) ? true : false;
-    return valid;
+      validLocation =
+        Math.floor(first / 10) == Math.floor(last / 10) ? true : false;
+
+    //Makes sure the ships don't overlap
+    const locations = [];
+    let unoccupied;
+    for (let i = 0; i < ship.length; i++) {
+      if (direction == "n") {
+        locations.push(initialLocation - i * 10);
+      }
+      if (direction == "e") {
+        locations.push(initialLocation + i);
+      }
+      if (direction == "s") {
+        locations.push(initialLocation + i * 10);
+      }
+      if (direction == "w") {
+        locations.push(initialLocation - i);
+      }
+    }
+    this.shipLocations.some((position) => locations.includes(position))
+      ? (unoccupied = false)
+      : (unoccupied = true);
+    return validLocation && unoccupied ? true : false;
   }
 
   addShip(initialLocation, direction, ship) {
     let currentLocation = initialLocation;
-    let valid = this.isValid(initialLocation, direction, ship);
-    let shipLocation = [];
+    let valid = this.isValidShip(initialLocation, direction, ship);
     if (valid) {
       for (let i = 0; i < ship.length; i++) {
         if (direction == "n") {
           this.board[currentLocation] = 1;
-          shipLocation.push(currentLocation);
+          this.shipLocations.push(currentLocation);
+          ship.location.push(currentLocation);
           currentLocation -= 10;
         }
         if (direction == "e") {
           this.board[currentLocation] = 1;
-          shipLocation.push(currentLocation);
+          this.shipLocations.push(currentLocation);
+          ship.location.push(currentLocation);
           currentLocation += 1;
         }
         if (direction == "s") {
           this.board[currentLocation] = 1;
-          shipLocation.push(currentLocation);
+          this.shipLocations.push(currentLocation);
+          ship.location.push(currentLocation);
           currentLocation += 10;
         }
         if (direction == "w") {
           this.board[currentLocation] = 1;
-          shipLocation.push(currentLocation);
+          this.shipLocations.push(currentLocation);
+          ship.location.push(currentLocation);
           currentLocation -= 1;
         }
       }
     }
-    return valid ? shipLocation : null;
+    this.allShips.push(ship);
+    return valid ? ship : null;
+  }
+
+  receiveAttack(x, y) {
+    const pos = x * 10 + y;
+    const hit = this.shipLocations.includes(pos);
+    if (hit) {
+      for (let i = 0; i < this.allShips.length; i++) {
+        const currentShip = this.allShips[i];
+        if (currentShip.location.includes(pos)) {
+          currentShip.shipHit();
+          if (currentShip.getHit() == currentShip.length) {
+            currentShip.updateStatus();
+          }
+        }
+      }
+    } else {
+      this.missed.push(pos);
+    }
+    return hit;
+  }
+
+  allSunk() {
+    const numberOfships = this.allShips.length;
+    let sunkShips = 0;
+    for (let i = 0; i < numberOfships; i++) {
+      const currentShip = this.allShips[i];
+      if (!currentShip.currentStatus()) {
+        sunkShips++;
+      }
+    }
+    return numberOfships == sunkShips ? true : false;
   }
 }
 
--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -3,32 +3,65 @@ import { Ship } from "../Ship/Ship";
 describe("Gameboard", () => {
   let newGameboard;
   let ship;
+  let ship2;
   beforeEach(() => {
     newGameboard = new Gameboard();
     ship = new Ship(3);
+    ship2 = new Ship(4);
+    newGameboard.addShip(99, "n", ship2);
   });
   test("Checks if gameboard is made", () => {
     expect(newGameboard.board.length).toEqual(100);
   });
-  test("isValid works", () => {
-    expect(newGameboard.isValid(20, "n", ship)).toBeTruthy();
-    expect(newGameboard.isValid(13, "n", ship)).toBeFalsy();
-    expect(newGameboard.isValid(79, "s", ship)).toBeTruthy();
-    expect(newGameboard.isValid(94, "s", ship)).toBeFalsy();
-    expect(newGameboard.isValid(53, "e", ship)).toBeTruthy();
-    expect(newGameboard.isValid(39, "e", ship)).toBeFalsy();
-    expect(newGameboard.isValid(94, "w", ship)).toBeTruthy();
-    expect(newGameboard.isValid(70, "w", ship)).toBeFalsy();
+  test("isValidShip works", () => {
+    expect(newGameboard.isValidShip(20, "n", ship)).toBeTruthy();
+    expect(newGameboard.isValidShip(13, "n", ship)).toBeFalsy();
+    expect(newGameboard.isValidShip(78, "s", ship)).toBeTruthy();
+    expect(newGameboard.isValidShip(94, "s", ship)).toBeFalsy();
+    expect(newGameboard.isValidShip(53, "e", ship)).toBeTruthy();
+    expect(newGameboard.isValidShip(39, "e", ship)).toBeFalsy();
+    expect(newGameboard.isValidShip(94, "w", ship)).toBeTruthy();
+    expect(newGameboard.isValidShip(70, "w", ship)).toBeFalsy();
+    expect(newGameboard.isValidShip(99, "w", ship)).toBeFalsy();
   });
   test("Add a valid ship", () => {
-    const shipLoc = newGameboard.addShip(32, "s", ship);
+    newGameboard.addShip(32, "s", ship);
     expect(newGameboard.board[32]).toEqual(1);
     expect(newGameboard.board[42]).toEqual(1);
     expect(newGameboard.board[52]).toEqual(1);
-    expect(shipLoc).toEqual([32, 42, 52]);
+    expect(ship.location).toEqual([32, 42, 52]);
   });
   test("Add an invalid ship", () => {
     const shipLoc = newGameboard.addShip(12, "n", ship);
     expect(shipLoc).toBeFalsy();
+  });
+});
+
+describe("Player actions", () => {
+  let newGameboard;
+  let ship, ship2;
+  beforeEach(() => {
+    newGameboard = new Gameboard();
+    ship = new Ship(3);
+    ship2 = new Ship(3);
+    newGameboard.addShip(32, "s", ship);
+    newGameboard.addShip(66, "s", ship2);
+  });
+
+  test("receiveAttack", () => {
+    expect(newGameboard.receiveAttack(3, 2)).toBeTruthy();
+    expect(newGameboard.receiveAttack(5, 3)).toBeFalsy();
+    expect(newGameboard.allShips[0].hit).toEqual(1);
+  });
+
+  test("Sink all ships", () => {
+    newGameboard.receiveAttack(3, 2);
+    newGameboard.receiveAttack(4, 2);
+    newGameboard.receiveAttack(5, 2);
+    newGameboard.receiveAttack(6, 6);
+    newGameboard.receiveAttack(7, 6);
+    expect(newGameboard.allSunk()).toBeFalsy();
+    newGameboard.receiveAttack(8, 6);
+    expect(newGameboard.allSunk()).toBeTruthy();
   });
 });
