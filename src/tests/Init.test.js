cde10434f253ea0e88b32960b35d5b3a23ece3ed
Kris Wong <krischwong@gmail.com>
2023-11-29 01:12:02 -0500

    No css testing

--- src/tests/Init.test.js
+++
@@ -1,9 +0,0 @@
-import Initialize from "../Initialize/Init";
-
-describe("Initialize HTML and CSS elements", () => {
-  beforeEach(() => {
-    Initialize();
-  });
-
-  test("The page is initialized", () => {});
-});
