be81ff6193b7ab6fe03169e9216063dd7cf0c42a
Kris Wong <krischwong@gmail.com>
2024-01-31 00:09:03 -0500

    New game now fixes background colour on squares.

--- src/modules/Update/Update.js
+++ src/modules/Update/Update.js
@@ -1,6 +1,9 @@
 const Update = (player) => {
   console.log(player);
-  const board = document.getElementById("player-board");
+  for (let i = 0; i < 99; i++) {
+    document.getElementById(`player-board-square-${i}`).style = "none";
+  }
+  // const board = document.getElementById("player-board");
   for (let i = 0; i < player.gameboard.shipLocations.length; i++) {
     const square = document.getElementById(
       `player-board-square-${player.gameboard.shipLocations[i]}`
