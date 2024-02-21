53ae8a6b9776f407651a0d7b3c8b3c8e4c144740
Kris Wong <krischwong@gmail.com>
2024-02-21 01:21:32 -0500

    See previous commit

--- src/modules/factories/Player/Player.js
+++ src/modules/factories/Player/Player.js
@@ -6,8 +6,8 @@ class Player {
     this.gameboard = new Gameboard();
   }
 
-  attack(x, y, gameboard) {
-    return gameboard.receiveAttack(x, y);
+  attack(x, y) {
+    return this.gameboard.receiveAttack(x, y);
   }
 
   randomAttack(gameboard) {
@@ -16,13 +16,6 @@ class Player {
     const x = Math.floor(Math.random() * 10);
     result = this.attack(x, y, gameboard);
     return result;
-  }
-
-  aiMove() {
-    let result;
-    do {
-      result = this.randomAttack(gameboard);
-    } while (result !== true || result !== false);
   }
 }
 
