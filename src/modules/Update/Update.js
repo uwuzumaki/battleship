621e65c368dda63aa09da4f4d2f321dd8aec7e74
Kris Wong <krischwong@gmail.com>
2024-02-20 23:26:02 -0500

    Changed update to respond to opponent board and player board separately

--- src/modules/Update/Update.js
+++ src/modules/Update/Update.js
@@ -1,14 +1,27 @@
-const Update = (player) => {
-  console.log(player);
-  for (let i = 0; i < 99; i++) {
-    document.getElementById(`player-board-square-${i}`).style = "none";
+const Update = (player, board) => {
+  if (board == 0) {
+    for (let i = 0; i <= 99; i++) {
+      document.getElementById(`player-div-child-square-${i}`).style = "none";
+    }
+    for (let i = 0; i < player.gameboard.shipLocations.length; i++) {
+      const square = document.getElementById(
+        `player-div-child-square-${player.gameboard.shipLocations[i]}`
+      );
+      console.log(square);
+      square.style.backgroundColor = "red";
+    }
   }
-  // const board = document.getElementById("player-board");
-  for (let i = 0; i < player.gameboard.shipLocations.length; i++) {
-    const square = document.getElementById(
-      `player-board-square-${player.gameboard.shipLocations[i]}`
-    );
-    square.style.backgroundColor = "red";
+  if (board == 1) {
+    console.log(player);
+    for (let i = 0; i <= 99; i++) {
+      document.getElementById(`opp-div-child-square-${i}`).style = "none";
+    }
+    for (let i = 0; i < player.gameboard.shipLocations.length; i++) {
+      const square = document.getElementById(
+        `opp-div-child-square-${player.gameboard.shipLocations[i]}`
+      );
+      square.style.backgroundColor = "red";
+    }
   }
 };
 
