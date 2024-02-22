0d25c839e48b1996dcdc81bec63b6f13cc6942b3
Kris Wong <krischwong@gmail.com>
2024-02-21 23:13:15 -0500

    Changed square colours based on board array values

--- src/modules/Update/Update.js
+++ src/modules/Update/Update.js
@@ -11,14 +11,21 @@ const Update = (player, board) => {
     }
   }
   if (board == 1) {
+    const pos = player.gameboard.board;
     for (let i = 0; i <= 99; i++) {
       document.getElementById(`opp-div-child-square-${i}`).style = "none";
     }
-    for (let i = 0; i < player.gameboard.shipLocations.length; i++) {
-      const square = document.getElementById(
-        `opp-div-child-square-${player.gameboard.shipLocations[i]}`
-      );
-      square.style.backgroundColor = "red";
+    for (let i = 0; i <= 99; i++) {
+      const square = document.getElementById(`opp-div-child-square-${i}`);
+      if (pos[i] == 1) {
+        square.style.backgroundColor = "green";
+      }
+      if (pos[i] == 2) {
+        square.style.backgroundColor = "white";
+      }
+      if (pos[i] == 3) {
+        square.style.backgroundColor = "red";
+      }
     }
   }
 };
