c7fb8faebf54f80ecb82a154f22f05a778df3457
Kris Wong <krischwong@gmail.com>
2024-02-22 00:17:01 -0500

    Lots of changes. Clicking on a square on the opponent board will now do one of several things:  - Check if the attack by the player is valid, if it is:    - It will add one to player goodHit    - Update the CPU display board    - Check if the player has won.    - Attack with CPU    - Add one to CPU goodHit    - Update the player display board    - Check if the CPU has won. If either side has won, it will remove the eventListener and print player win to console.

--- src/modules/GameController/GameController.js
+++ src/modules/GameController/GameController.js
@@ -1,14 +1,36 @@
+import Update from "../Update/Update";
+
 const GameController = (combined) => {
   const opponentBoard = document.getElementById("opp-div-child");
   let player = combined[0];
   let cpu = combined[1];
 
   opponentBoard.addEventListener("click", attack);
+
   function attack(e) {
-    console.log(e.target);
-    console.log(player.gameboard.shipLocations);
-    console.log(cpu.gameboard.shipLocations);
+    let pos = parseInt(e.target.id.match(/\d+/g)[0]);
+    let result = cpu.attack(pos);
+    if (result != null) {
+      result ? player.goodHit() : null;
+      Update(cpu, 1);
+      player.checkWin() ? endGame(0) : null;
+
+      let result2 = player.randomAttack();
+      result2 ? cpu.goodHit() : null;
+      Update(player, 0);
+      cpu.checkWin() ? endGame(1) : null;
+    }
   }
+
+  const endGame = (player) => {
+    opponentBoard.removeEventListener("click", attack);
+    if (player == 0) {
+      console.log("Player Wins");
+    }
+    if (player == 1) {
+      console.log("Cpu wins");
+    }
+  };
 };
 
 export default GameController;
