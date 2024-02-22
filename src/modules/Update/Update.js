b9a9776ba5a44fd9177e5f3b8e84da55e4d170a8
Kris Wong <krischwong@gmail.com>
2024-02-22 00:28:16 -0500

    Added colours based on board array state for player

--- src/modules/Update/Update.js
+++ src/modules/Update/Update.js
@@ -1,13 +1,20 @@
 const Update = (player, board) => {
   if (board == 0) {
+    const pos = player.gameboard.board;
     for (let i = 0; i <= 99; i++) {
       document.getElementById(`player-div-child-square-${i}`).style = "none";
     }
-    for (let i = 0; i < player.gameboard.shipLocations.length; i++) {
-      const square = document.getElementById(
-        `player-div-child-square-${player.gameboard.shipLocations[i]}`
-      );
-      square.style.backgroundColor = "red";
+    for (let i = 0; i <= 99; i++) {
+      const square = document.getElementById(`player-div-child-square-${i}`);
+      if (pos[i] == 1) {
+        square.style.backgroundColor = "green";
+      }
+      if (pos[i] == 2) {
+        square.style.backgroundColor = "white";
+      }
+      if (pos[i] == 3) {
+        square.style.backgroundColor = "red";
+      }
     }
   }
   if (board == 1) {
@@ -17,9 +24,10 @@ const Update = (player, board) => {
     }
     for (let i = 0; i <= 99; i++) {
       const square = document.getElementById(`opp-div-child-square-${i}`);
-      if (pos[i] == 1) {
-        square.style.backgroundColor = "green";
-      }
+      // Makes enemy ships visible
+      // if (pos[i] == 1) {
+      //   square.style.backgroundColor = "green";
+      // }
       if (pos[i] == 2) {
         square.style.backgroundColor = "white";
       }
