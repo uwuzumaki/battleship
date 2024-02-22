8ac81ec962366fc70120e633182f04f5896166a0
Kris Wong <krischwong@gmail.com>
2024-02-22 00:13:49 -0500

    Lots of changes randomAttack will attack until a valid attack is made until it's valid then return true. goodHit tracks when the player makes a hit. checkWin will check if the player has won the game.

--- src/modules/factories/Player/Player.js
+++ src/modules/factories/Player/Player.js
@@ -4,18 +4,29 @@ class Player {
   constructor() {
     this.ships = [];
     this.gameboard = new Gameboard();
+    this.enemyHit = 0;
   }
 
-  attack(x, y) {
-    return this.gameboard.receiveAttack(x, y);
+  attack(pos) {
+    return this.gameboard.receiveAttack(pos);
   }
 
-  randomAttack(gameboard) {
+  randomAttack() {
     let result;
-    const y = Math.floor(Math.random() * 10);
-    const x = Math.floor(Math.random() * 10);
-    result = this.attack(x, y, gameboard);
+    do {
+      const pos = Math.floor(Math.random() * 100);
+      result = this.gameboard.receiveAttack(pos);
+    } while (result == null);
     return result;
+  }
+
+  goodHit() {
+    this.enemyHit++;
+    console.log(this.enemyHit);
+  }
+
+  checkWin() {
+    return this.enemyHit == 17;
   }
 }
 
